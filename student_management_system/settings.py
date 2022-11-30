c7b7d535caab31b6e54e4b4523725d6867668374
trieulv <trieulv@ftech.ai>
2022-11-30 14:18:00 +0700

    Set up mongodb

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import dj_database_url
 import os
+import mongoengine
 from pathlib import Path
 
 
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('MY_SECRET_KEY')  # Consider using your secret key
+SECRET_KEY = os.environ.get('MY_SECRET_KEY','local')  # Consider using your secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -100,6 +101,10 @@ DATABASES = {
     #     'HOST': '127.0.0.1',
     #     'PORT': '3307'
     # }
+#     "default": {
+#       "ENGINE": "djongo",
+#       "NAME": "your-db-name",
+#    }
 }
 
 
